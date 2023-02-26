3711ced3f4bc1977e04c555d01f6a071cc63af41
Adrien Driffaud <adrien@driffaud.fr>
2023-02-26 14:25:34 +0100

    Add Tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/login">Login</a>
+</nav>
+
+<slot />
+
+<footer>
+	Made with ❤️ with <a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">SvelteKit</a>,
+	<a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind CSS</a>
+	&
+	<a href="https://pocketbase.io" target="_blank" rel="noreferrer">PocketBase</a>
+</footer>
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,3 @@
+export function load() {
+	return { items: [{ name: 'Toto' }] };
+}
