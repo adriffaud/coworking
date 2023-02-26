504c1ec6c4d45cb41a8f322be4afb052f55687fd
Adrien Driffaud <adrien@driffaud.fr>
2023-02-26 14:31:27 +0100

    Actually, no Tailwind for now...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,3 @@
-<script>
-	import '../app.css';
-</script>
-
 <nav>
 	<a href="/">Home</a>
 	<a href="/login">Login</a>
@@ -10,8 +6,6 @@
 <slot />
 
 <footer>
-	Made with ❤️ with <a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">SvelteKit</a>,
-	<a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind CSS</a>
-	&
-	<a href="https://pocketbase.io" target="_blank" rel="noreferrer">PocketBase</a>
+	Made with ❤️ with <a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">SvelteKit</a>
+	& <a href="https://pocketbase.io" target="_blank" rel="noreferrer">PocketBase</a>
 </footer>
